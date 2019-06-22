042cb9c62464e64e7ab0ae7254af6f7b0b562605
ONDRUS, KERI M [AG-Contractor/1000] <keri.m.ondrus@monsanto.com>
2019-06-22 11:04:13 -0500

    removed boilerplate react app and added assists container

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/App.css';
+import AssistAppContainer from "./Containers/AssistAppContainer";
+
+function App() {
+  return (
+    <div className="App">
+      <AssistAppContainer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Containers/AssistAppContainer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AssistAppContainer = () => {
+  return (
+    <React.Fragment>
+      This is the AssistAppContainer
+    </React.Fragment>
+  )
+};
+
+export default AssistAppContainer;
