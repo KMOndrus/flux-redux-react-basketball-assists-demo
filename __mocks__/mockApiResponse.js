9c547ad92adc93661b19e08c9868d530a4ebfdf0
ONDRUS, KERI M [AG-Contractor/1000] <keri.m.ondrus@monsanto.com>
2019-06-22 13:14:02 -0500

    adding mock api response

---
+++ __mocks__/mockApiResponse.js
@@ -0,0 +1,64 @@
+export const players = {
+  payload: [
+    {
+      "player": "Lebron",
+      "assistStats": [
+        {
+          "year": 2018,
+          "regSeason": 747,
+          "playoffs": 198,
+        },
+        {
+          "year": 2017,
+          "regSeason": 646,
+          "playoffs": 141,
+        },
+        {
+          "year": 2016,
+          "regSeason": 514,
+          "playoffs": 160,
+        },
+        {
+          "year": 2015,
+          "regSeason": 511,
+          "playoffs": 169,
+        },
+        {
+          "year": 2014,
+          "regSeason": 488,
+          "playoffs": 95,
+        },
+      ]
+    },
+    {
+      "player": "MJ",
+      "assistStats": [
+        {
+          "year": 1998,
+          "regSeason": 283,
+          "playoffs": 74,
+        },
+        {
+          "year": 1997,
+          "regSeason": 352,
+          "playoffs": 91,
+        },
+        {
+          "year": 1996,
+          "regSeason": 252,
+          "playoffs": 74,
+        },
+        {
+          "year": 1995,
+          "regSeason": 90,
+          "playoffs": 45,
+        },
+        {
+          "year": 1993,
+          "regSeason": 428,
+          "playoffs": 114,
+        },
+      ]
+    }
+  ]
+};
